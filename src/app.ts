0823eef6f4b39259b05a902f4d3aae627156545c
drkeck <k3ckl3y.a@gmail.com>
2023-04-29 05:36:17 -0400

    feat: update infistructure but it is not working currently

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+// import inquirer from 'inquirer';
 
 const server = express();
 const port = 3000;
 
 function app() {
     server.listen(port, () => {
-        console.log(`listening on port ${port}`)
+        console.log(`listening on port ${port}`);
+        // inquirer
+        //     .prompt([{
+        //         type: 'list',
+        //         name: 'userInput',
+        //         message: 'what example would you like to visit',
+        //         choices: ['math', 'misc functions', 'strings']
+        //     }])
+
     })
 }
 
